08f261791f7689278c124af12085505c9db684c1
james424smith@gmail.com <james424smith@gmail.com>
2021-01-04 04:24:08 +0900

    Resume and pause implemented

--- src/reducers/game-reducer.js
+++ src/reducers/game-reducer.js
@@ -69,11 +69,11 @@ const gameReducer = (state = defaultState(), action) => {
 
     case RESUME:
 
-      return state
+      return { ...state, isRunning: true }
 
     case PAUSE:
 
-      return state
+      return { ...state, isRunning: false }
 
     case GAME_OVER:
 
